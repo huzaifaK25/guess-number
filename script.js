b0dac610e48b63a7ff8c8a5610493d3f35eb73d5
DESKTOP-BKI77CE\kashi <huzaifakashif202@gmail.com>
2025-02-25 20:20:31 +0500

    added JS functionalty

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 'use strict';
+
+let score = 20;
+let secretNum = Math.floor(Math.random() * 20 + 1);
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  // no input
+  if (!guess) {
+    displayMessage('No Number!');
+  }
+  //player wins the game
+  else if (guess === secretNum) {
+    displayMessage('Correct Number!');
+    document.querySelector('.number').textContent = secretNum;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNum) {
+    if (score > 1) {
+      displayMessage(guess > secretNum ? 'Too High!' : 'Too Low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You Lost!');
+      document.querySelector('.score').textContent = '0';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNum = Math.floor(Math.random() * 20 + 1);
+  displayMessage('Start Guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
